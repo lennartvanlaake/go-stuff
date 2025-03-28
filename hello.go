2fe65101ac3ab249d14a8c8c6da68c9fcf7594c6
Lennart van Laake <lennartvanlaake@gmail.com>
2025-03-28 13:53:24 +0100

    templates and sql work

--- hello.go
+++ hello.go
@@ -1,23 +1,29 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/glebarez/go-sqlite"
 )
 
 // https://gin-gonic.com/docs/examples/html-rendering
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	db, err := sql.Open("sqlite", ":memory:")
+	if err != nil {
+		log.Fatal(err)
+	}
 	router.GET("/index", func(c *gin.Context) {
+		var version string
+		row := db.QueryRow("select sqlite_version()")
+		row.Scan(&version)
+		log.Print(version)
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"name": "Lennart",
+			"name": version,
 		})
 	})
 	router.Run()
