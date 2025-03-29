462c724ab77d59de0f2aadf1b40c2bda85eed553
Lennart van Laake <lennartvanlaake@gmail.com>
2025-03-29 13:14:52 +0100

    working todo-list app

--- hello.go
+++ hello.go
@@ -2,22 +2,33 @@ package main
 
 import (
 	"database/sql"
-	"htmx-server/renderer"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/glebarez/go-sqlite"
+	"github.com/htmx-poc/renderer"
+	. "github.com/htmx-poc/templates"
+	. "github.com/htmx-poc/types"
 )
 
-type User struct {
-	Name string
+func getUsers(db *sql.DB) []User {
+	var users []User
+	rows, err := db.Query("SELECT * from users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		var user User
+		rows.Scan(&user.Name)
+		users = append(users, user)
+	}
+	return users
 }
 
 // https://gin-gonic.com/docs/examples/html-rendering
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
 	db, err := sql.Open("sqlite", ":memory:")
 	ginHtmlRenderer := router.HTMLRender
 	router.HTMLRender = &renderer.HTMLTemplRenderer{FallbackHtmlRenderer: ginHtmlRenderer}
@@ -37,26 +48,20 @@ func main() {
 	})
 
 	router.GET("/admin", func(c *gin.Context) {
-		var users []User
-		rows, _ := db.Query("SELECT * from users")
-		for rows.Next() {
-			var user User
-			rows.Scan(&user.Name)
-			log.Print(user.Name)
-			users = append(users, user)
-		}
-		c.HTML(http.StatusOK, "admin.tmpl", gin.H{
-			"users": users,
-		})
+		c.HTML(http.StatusOK, "", Admin(getUsers(db)))
 	})
 
 	router.POST("/admin", func(c *gin.Context) {
+		c.Request.ParseForm()
 		name := c.PostForm("fname")
-		log.Print("Processing the post")
-		log.Print(name)
 		db.Exec("INSERT INTO users VALUES (?)", name)
-		c.Redirect(http.StatusFound, "/admin")
+		c.HTML(http.StatusOK, "", UserList(getUsers(db)))
+	})
 
+	router.DELETE("/users/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		db.Exec("DELETE FROM users WHERE name = (?)", name)
+		c.HTML(http.StatusOK, "", UserList(getUsers(db)))
 	})
 
 	router.Run()
